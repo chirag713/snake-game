289ce86b5c4d9942bec11cb688358311e4f7c30f
chirag <126777836+chirag713@users.noreply.github.com>
2023-12-25 15:25:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const gameoversound = new Audio('./music/gameover.mp3');
 const movesound = new Audio('./music/move.mp3');
 const musicsound = new Audio('./music/music.mp3');
 let board = document.getElementById("board");
+let up=document.getElementById("up");
+// let up=document.getElementByid("up");
+let down=document.getElementById("down");
+let right=document.getElementById("right");
+let left=document.getElementById("left");
 
-let score=0;
+let score = 0;
 
 let speed = 5;
 let lastpainttime = 0;
@@ -33,17 +38,17 @@ function main(ctime) {
     gameengine();
 }
 
-function iscollide(snake){
+function iscollide(snake) {
     //if u go into yoursel
-    for(let i=1;i<snakearr.length;i++){
-        if(snake[i].x===snake[0].x && snake[i].y===snake[0].y){
+    for (let i = 1; i < snakearr.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) {
             return true;
         }
-        
+
     }
 
     //if it get stuck into wall
-    if(snake[0].x>=18 ||snake[0].x<=0 ||snake[0].y>=18 || snake[0].y<=0){
+    if (snake[0].x >= 18 || snake[0].x <= 0 || snake[0].y >= 18 || snake[0].y <= 0) {
         return true;
     }
     return false;
@@ -52,39 +57,46 @@ function iscollide(snake){
 function gameengine() {
     //updating sanke array and food
 
-    if(iscollide(snakearr)){
+    if (iscollide(snakearr)) {
         gameoversound.play();
         musicsound.pause();
         movesound.pause();
-        inputdir={x:0,y:0};
+        inputdir = { x: 0, y: 0 };
         alert("Game over press any key to play again");
-        snakearr=[
+        snakearr = [
             { x: 13, y: 15 }
         ]
         musicsound.play();
-        score=0;
+        let chirag = document.getElementById("highestscore");
+        if (chirag.innerText < score)
+            chirag.innerText = score;
+        document.getElementById("score").innerText=0
+        score = 0;
     }
 
 
     //if you have eaten the food increment score and add new food
 
-    if(snakearr[0].y===food.y && snakearr[0].x===food.x){
+    if (snakearr[0].y === food.y && snakearr[0].x === food.x) {
         foodsound.play();
         score++;
-        snakearr.unshift({x:snakearr[0].x+inputdir.x,y:snakearr[0].y+inputdir.y});
-        let a=2;
-        let b=16;
-        food={x:Math.round(a+(b-a)*Math.random()),y:Math.round(a+(b-a)*Math.random())};
+        snakearr.unshift({ x: snakearr[0].x + inputdir.x, y: snakearr[0].y + inputdir.y });
+        let a = 2;
+        let b = 16;
+        food = { x: Math.round(a + (b - a) * Math.random()), y: Math.round(a + (b - a) * Math.random()) };
+        document.getElementById("score").innerText = score;
+        
+
     }
 
     //move the snake
 
-    for(let i=snakearr.length-2;i>=0;i--){
-        snakearr[i+1]={...snakearr[i]};
+    for (let i = snakearr.length - 2; i >= 0; i--) {
+        snakearr[i + 1] = { ...snakearr[i] };
     }
 
-    snakearr[0].x+=inputdir.x;
-    snakearr[0].y+=inputdir.y;
+    snakearr[0].x += inputdir.x;
+    snakearr[0].y += inputdir.y;
 
     //rendering the snake array 
     board.innerHTML = "";
@@ -129,27 +141,55 @@ window.addEventListener('keydown', e => {
     // console.log(e.key);
     switch (e.key) {
         case "ArrowUp":
-            console.log("arrow up");
-            inputdir.x=0;
-            inputdir.y=-1;
+            inputdir.x = 0;
+            inputdir.y = -1;
             break;
         case "ArrowDown":
-            console.log("arrow down");
-            inputdir.x=0;
-            inputdir.y=1;
+            inputdir.x = 0;
+            inputdir.y = 1;
             break;
         case "ArrowRight":
-            console.log("arrow right");
-            inputdir.x=1;
-            inputdir.y=0;
+            inputdir.x = 1;
+            inputdir.y = 0;
             break;
         case "ArrowLeft":
-            console.log("arrow left");
-            inputdir.x=-1;
-            inputdir.y=0;
+            inputdir.x = -1;
+            inputdir.y = 0;
             break;
 
         default:
             break;
     }
 })
+
+left.addEventListener("click", leftmove);
+right.addEventListener("click", rightmove);
+up.addEventListener("click", upmove);
+down.addEventListener("click", downmove);
+
+
+
+
+function upmove(){
+    movesound.play();
+    inputdir.x = 0;
+    inputdir.y = -1;
+}
+
+function downmove(){
+    movesound.play();
+    inputdir.x = 0;
+    inputdir.y = 1;
+}
+
+function rightmove(){
+    movesound.play();
+    inputdir.x = 1;
+    inputdir.y = 0;
+}
+
+function leftmove(){
+    movesound.play();
+    inputdir.x = -1;
+    inputdir.y = 0;
+}
